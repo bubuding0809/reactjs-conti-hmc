8d141c8b114d2762d99e5862daf223cf402d2ada
bubuding0809 <bubuding0809@gmail.com>
2022-08-01 10:55:46 +0800

    Implemented client to client web calling

--- client/src/components/VoiceCaller.js
+++ client/src/components/VoiceCaller.js
@@ -12,6 +12,7 @@ import CallIcon from "@mui/icons-material/Call";
 import CallEndIcon from "@mui/icons-material/CallEnd";
 import MicIcon from "@mui/icons-material/Mic";
 import PhoneInTalkIcon from "@mui/icons-material/PhoneInTalk";
+import WifiCalling3Icon from "@mui/icons-material/WifiCalling3";
 import axios from "axios";
 
 export default function VoiceCaller() {
@@ -19,58 +20,41 @@ export default function VoiceCaller() {
   const [phoneNumber, setPhoneNumber] = useState("");
   const [callDevice, setCallDevice] = useState(null);
   const [callInstance, setCallInstance] = useState(null);
+  const [clientIdentity, setClientIdentity] = useState("");
+  const [isIncomingCall, setIsIncomingCall] = useState(false);
   const [callAudio, setCallAudio] = useState({
     inputVol: 0,
     outputVol: 0,
   });
 
-  function addDeviceListeners(device) {
-    device.on("registered", () =>
-      console.log("The device is ready to receive incoming calls.")
-    );
-    device.on("error", error => {
-      console.log("Twilio.Device Error: " + error);
-    });
-  }
+  async function startupClient() {
+    console.log("Requesting access token...");
 
-  function addCallListeners(call) {
-    call.on("accept", () => console.log("Call connected"));
-    call.on("cancel", () => console.log("Call cancelled"));
-    call.on("error", error => console.log("Call error", error));
-    call.on("volume", (inputVol, outputVol) =>
-      setCallAudio({ inputVol, outputVol })
-    );
-    call.on("disconnect", () => {
-      callDevice.destroy();
-      setCallDevice(null);
-      setCallInstance(null);
-      setIsDeviceReady(false);
-      setCallAudio({ inputVol: 0, outputVol: 0 });
-    });
-  }
-
-  function handleHangUp() {
-    if (!callInstance) {
-      console.log("No call to hangup");
-      return;
-    }
-
-    callInstance.disconnect();
-  }
-
-  async function setupDevice() {
     try {
       // Get the device token from the server
       const response = await axios.get("/twilio_voice/token");
+      console.log("Got access token");
       const token = response.data.token;
+      console.log(response);
+
+      // Display client name on UI
+      setClientIdentity(response.data.identity);
 
       // Create a Twilio.Device instance with token
+      console.log("Initializing device");
+
       const device = new Device(token, {
+        logLevel: 1,
+        // Set Opus as our preferred codec. Opus generally performs better, requiring less bandwidth and
+        // providing better audio quality in restrained network conditions.
         codecPreferences: ["opus", "pcmu"],
       });
-
+      console.log(device);
       // Add listeners for device events
       addDeviceListeners(device);
+
+      // Device must be registered in order to receive incoming calls
+      device.register();
 
       //Update state
       setCallDevice(device);
@@ -83,12 +67,51 @@ export default function VoiceCaller() {
     }
   }
 
+  function addDeviceListeners(device) {
+    device.on("registered", () =>
+      console.log("The device is ready to receive incoming calls.")
+    );
+    device.on("error", error => {
+      console.log("Twilio.Device Error: " + error);
+    });
+    device.on("incoming", call => {
+      console.log(call);
+      console.log(`Incoming call from ${call.parameters.From}`);
+      setIsIncomingCall(true);
+
+      // add event listener to call object
+      addCallListeners(call);
+
+      setCallInstance(call);
+    });
+  }
+
+  function addCallListeners(call) {
+    call.on("accept", () => console.log("Call connected"));
+    call.on("cancel", () => console.log("Call cancelled"));
+    call.on("reject", () => console.log("Call rejected."));
+    call.on("error", error => console.log("Call error", error));
+    call.on("volume", (inputVol, outputVol) =>
+      setCallAudio({ inputVol, outputVol })
+    );
+    call.on("disconnect", () => {
+      console.log("Call disconnected");
+      callDevice.destroy();
+      setCallDevice(null);
+      setCallInstance(null);
+      setIsDeviceReady(false);
+      setClientIdentity("");
+      setCallAudio({ inputVol: 0, outputVol: 0 });
+    });
+  }
+
   async function handleOutgoingCall() {
     if (!isDeviceReady) {
       console.log("call device not setup");
       return;
     }
 
+    console.log("Attempting to call" + phoneNumber);
     try {
       const call = await callDevice.connect({
         params: {
@@ -102,6 +125,36 @@ export default function VoiceCaller() {
     }
   }
 
+  function acceptIncomingCall() {
+    if (!callInstance) {
+      console.log("No call to accept");
+      return;
+    }
+    setIsIncomingCall(false);
+    callInstance.accept();
+    console.log("Accepted incoming call");
+  }
+
+  function rejectIncomingCall() {
+    if (!callInstance) {
+      console.log("No call to reject");
+      return;
+    }
+    setIsIncomingCall(false);
+    callInstance.reject();
+    console.log("Rejected incoming call");
+  }
+
+  function hangupCall() {
+    if (!callInstance) {
+      console.log("No call to hangup");
+      return;
+    }
+    setIsIncomingCall(false);
+    callInstance.disconnect();
+    console.log("Hanging up incoming call");
+  }
+
   return (
     <Grid
       container
@@ -113,8 +166,25 @@ export default function VoiceCaller() {
       rowGap={1}
     >
       <Typography variant="body1">Voice Caller</Typography>
-      <Grid item xs={12}>
-        <Button onClick={setupDevice}>Setup device</Button>
+      <Grid item container xs={12} alignItems="center">
+        <Grid item xs="auto">
+          <Button onClick={startupClient}>Setup device</Button>
+        </Grid>
+        <Grid item xs>
+          <Typography variant="body2" fontSize={13}>
+            {clientIdentity}
+          </Typography>
+        </Grid>
+        {isIncomingCall && (
+          <>
+            <Grid item xs="auto">
+              <Button onClick={acceptIncomingCall}>Accept</Button>
+            </Grid>
+            <Grid item xs="auto">
+              <Button onClick={rejectIncomingCall}>Reject</Button>
+            </Grid>
+          </>
+        )}
       </Grid>
       <Grid item xs mr={2}>
         <TextField
@@ -134,7 +204,7 @@ export default function VoiceCaller() {
         </Fab>
       </Grid>
       <Grid item xs="auto">
-        <Fab color="error" onClick={handleHangUp} disabled={!isDeviceReady}>
+        <Fab color="error" onClick={hangupCall} disabled={!isDeviceReady}>
           <CallEndIcon />
         </Fab>
       </Grid>
@@ -145,7 +215,8 @@ export default function VoiceCaller() {
         <Grid item xs>
           <LinearProgress
             sx={{
-              height: 10,
+              height: 20,
+              borderRadius: 1.5,
             }}
             color="primary"
             variant="determinate"
@@ -160,7 +231,8 @@ export default function VoiceCaller() {
         <Grid item xs>
           <LinearProgress
             sx={{
-              height: 10,
+              height: 20,
+              borderRadius: 1.5,
             }}
             color="secondary"
             variant="determinate"
--- routes/twilio_voice.js
+++ routes/twilio_voice.js
@@ -1,20 +1,43 @@
 const express = require("express");
 const router = express.Router();
 const twilio = require("twilio");
-// const ClientCapability = twilio.jwt.ClientCapability;
 const VoiceResponse = twilio.twiml.VoiceResponse;
 const AccessToken = require("twilio").jwt.AccessToken;
 const VoiceGrant = AccessToken.VoiceGrant;
+// const { v4: uuidv4 } = require("uuid");
+const nameGenerator = require("../name_generator");
 
 require("dotenv").config();
 
-// Generate a Twilio Client capability token
+var identity;
+
+/**
+ * Checks if the given value is valid as phone number
+ * @param {Number|String} number
+ * @return {Boolean}
+ */
+function isAValidPhoneNumber(number) {
+  // Check if the given value is a valid phone number
+  // return true if it is, false otherwise
+  return /^[\d\+\-\(\) ]+$/.test(number);
+}
+
+// Generate an access token for a Twilio Client Application
 router.get("/token", (req, res) => {
+  // Generate a unique identifier for the client
+  identity = nameGenerator();
+
+  // Generate an access token that we will sign and return to the client
   const accessToken = new AccessToken(
     process.env.TWILIO_ACCOUNT_SID,
     process.env.TWILIO_API_KEY_SID,
-    process.env.TWILIO_API_KEY_SECRET
+    process.env.TWILIO_API_KEY_SECRET,
+    { identity: identity }
   );
+
+  // // Assign a unqiue identity to the access token
+  // accessToken.identity = identity;
+
   const grant = new VoiceGrant({
     outgoingApplicationSid: process.env.TWILIO_TWIML_APP_SID,
     incomingAllow: true,
@@ -23,27 +46,38 @@ router.get("/token", (req, res) => {
 
   // Include token in a JSON response
   res.send({
+    identity: identity,
     token: accessToken.toJwt(),
   });
 });
 
 // Create TwiML for outbound calls
 router.post("/voice", (req, res) => {
-  const toNumber = req.body.To;
+  const reciever = req.body.To;
   const callerId = process.env.TWILIO_NUMBER;
   const response = new VoiceResponse();
 
   // If the request to the /voice endpoint is TO your Twilio Number,
   // then it is an incoming call towards your Twilio.Device.
-  if (toNumber) {
+  if (reciever === callerId) {
+    console.log("Incoming call from " + reciever);
+    const dial = response.dial();
+
+    dial.client(identity);
+  } else if (reciever) {
+    console.log("Outgoing call to " + reciever);
     // set the callerId
     const dial = response.dial({ callerId });
-    dial.number(toNumber);
 
-    // dial["number"]({}, toNumber);
+    // Check if the 'To' parameter is a Phone Number or Client Name
+    // in order to use the appropriate TwiML noun
+    const attr = isAValidPhoneNumber(reciever) ? "number" : "client";
+    console.log(attr);
+    dial[attr]({}, reciever);
   } else {
+    console.log("No reciever specified");
     response.say(
-      "Thanks for calling, this is an automated message. Thank you for your email. I will be out of the office from mm/dd to mm/dd and will have limited access to email / will not have access to email. If this is urgent, please contact [NAME] at [EMAIL] or [PHONE]. I will do my best to respond promptly to your email when I return on mm/dd."
+      "Thanks for calling, this is an automated message. You have not entered a phone number while dialing hence you are hearing this. This is a automated test message. Thank you for your call. "
     );
   }
 
